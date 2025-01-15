32c94b699fd332c1bf0f6c9a5d80b22b04c97762
llucas.spot <llucas.spot@gmail.com>
2025-01-15 22:38:30 +0100

    add: JwkFromPublicKeyPemExtractorForgeNodeAdapter

---
+++ src/shared/jwks/modules/local/infrastructure/adapters/jwk-from-public-key-pem.extractor.forge-node-adapter.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { pki } from 'node-forge';
+import { ArrayIncludes } from '../../../../../core/array.utils';
+import {
+  AvailableAlgorithm,
+  Jwk,
+  JwkFromPublicKeyPemExtractorPort,
+} from '../../domain/jwk-from-public-key-pem.extractor.port';
+
+@Injectable()
+export class JwkFromPublicKeyPemExtractorForgeNodeAdapter
+  implements JwkFromPublicKeyPemExtractorPort
+{
+  private readonly logger = new Logger(this.constructor.name);
+
+  async extractFrom({
+    publicKeyPem,
+    alg,
+  }: {
+    publicKeyPem: string;
+    alg: AvailableAlgorithm;
+  }): Promise<Jwk> {
+    if (!ArrayIncludes(['RS256', 'RS384', 'RS512'] as const, alg)) {
+      this.logger.error('forge-node only support RSA');
+      throw new InternalServerErrorException();
+    }
+
+    const jwk = pki.publicKeyFromPem(publicKeyPem);
+    const n = Buffer.from(jwk.n.toByteArray()).toString('base64url');
+    const e = Buffer.from(jwk.e.toByteArray()).toString('base64url');
+
+    return {
+      kty: 'RSA',
+      ...jwk,
+      n,
+      e,
+      alg,
+      use: 'sig',
+      kid: '1',
+    };
+  }
+}
--- src/shared/jwks/modules/local/infrastructure/jwks.module.local.ts
+++ src/shared/jwks/modules/local/infrastructure/jwks.module.local.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthConfModule } from '../../../../../services/auth/modules/local/infrastructure/auth-conf.module';
+import { inWebContainer } from '../../../../hashing/infrastructure/in-web-container';
 import { JwksServicePort } from '../../../domain/jwks.service.port';
 import { JwksServiceLocalAdapter } from '../application/jwks.service.local-adapter';
 import { PublicKeyPemGetter } from '../application/public-key-pem.getter';
 import { JwkFromPublicKeyPemExtractorPort } from '../domain/jwk-from-public-key-pem.extractor.port';
-import { JwkFromPublicKeyPemExtractorJoseAdapter } from './adapters/jwk-from-public-key-pem.extractor.jose-adapter';
 
 @Module({
   imports: [AuthConfModule],
@@ -16,7 +16,23 @@ import { JwkFromPublicKeyPemExtractorJoseAdapter } from './adapters/jwk-from-pub
     PublicKeyPemGetter,
     {
       provide: JwkFromPublicKeyPemExtractorPort,
-      useClass: JwkFromPublicKeyPemExtractorJoseAdapter,
+      useFactory: (): Promise<JwkFromPublicKeyPemExtractorPort> => {
+        return inWebContainer<JwkFromPublicKeyPemExtractorPort>({
+          loadIfTrue: async () => {
+            const { JwkFromPublicKeyPemExtractorForgeNodeAdapter } =
+              await import(
+                './adapters/jwk-from-public-key-pem.extractor.forge-node-adapter'
+              );
+            return new JwkFromPublicKeyPemExtractorForgeNodeAdapter();
+          },
+          loadIfFalse: async () => {
+            const { JwkFromPublicKeyPemExtractorJoseAdapter } = await import(
+              './adapters/jwk-from-public-key-pem.extractor.jose-adapter'
+            );
+            return new JwkFromPublicKeyPemExtractorJoseAdapter();
+          },
+        });
+      },
     },
   ],
   exports: [JwksServicePort],
